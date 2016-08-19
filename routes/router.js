74efa101d1ec4b0b456791e51e1cd877a9f71189
Sean Hornsby <sean.hornsby@gmail.com>
2016-08-18 20:46:30 -0700

    Adds get route for routific routes.

--- routes/router.js
+++ routes/router.js
@@ -1,18 +1,35 @@
 var router = require('express').Router();
+var request = require('request');
+var data = require('../data/data.json');
+var auth = `bearer ${process.env.ROUTIFIC_KEY}`;
+var options = {
+  url: 'https://api.routific.com/v1/vrp',
+  json: data,
+  headers: {
+    'Authorization': auth
+  }
+};
 
 router.post('/vehicle', function(req, res) {
-  console.log('vehicle post');
   res.send();
 });
 
 router.post('/stop', function(req, res) {
-  console.log('stop post');
   res.send();
 });
 
 router.get('/optimized', function(req, res) {
-  console.log('get optimized');
-  res.send();
+  request.post(options, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+        res.send(body);
+      } else {
+        console.log(`${response.statusCode} : ${body.error}`);
+  }
+  });
+
+  
 });
+
 
 module.exports = router;
